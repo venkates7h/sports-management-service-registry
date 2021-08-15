328cee9596d1de176dc2c0d5ab8f0dbec31b8c0c
venkates7h <venkates7.h@gmail.com>
2021-08-15 16:18:04 +0530

    first commit

---
+++ src/main/java/com/sports/management/system/SportsManagementSystemRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.sports.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Configuration
+public class SportsManagementSystemRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsManagementSystemRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sports/management/system/SportsManagementSystemRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsManagementSystemRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
